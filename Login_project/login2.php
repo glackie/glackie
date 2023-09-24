adc4ca7771eb7868a24261ab30aaa812ac489ee3
chirag <glack@localhost.localdomain>
2023-09-24 21:28:09 +0530

    successful login php

--- Login_project/login2.php
+++ Login_project/login2.php
@@ -1,41 +1,52 @@
-<?php  
-$db_host = 'localhost';
-$db_user = 'root';
-$db_password = 'glack';
-$db_name = 'creds';
+<html>
+    <head>
+        <title>
+            Successful login authentication
+        </title>
+    </head>
+    <body>
+        
+    
 
-$conn = mysqli_connect($db_host, $db_user, $db_password, $db_name);
+    <?php  
+        $db_host = 'localhost';
+        $db_user = 'glackboi';
+        $db_password = 'glackboipass';
+        $db_name = 'users';
 
-if (!$conn) {
-    die("Connection failed: " . mysqli_connect_error());
-}
+        $conn = mysqli_connect($db_host, $db_user, $db_password, $db_name);
 
-$username = $_POST['uname'];
-$password = $_POST['pass'];
+        if (!$conn) {
+            die("Connection failed: " . mysqli_connect_error());
+        }
 
-$query = "SELECT * FROM users WHERE username = ?";
-$stmt = mysqli_prepare($conn, $query);
+        $username = $_POST['uname'];
+        $password = $_POST['pass'];
 
-if ($stmt) {
-    mysqli_stmt_bind_param($stmt, "s", $username);
-    mysqli_stmt_execute($stmt);
-    $result = mysqli_stmt_get_result($stmt);
-}
+        $query = "SELECT * FROM creds WHERE username = ? AND password = ?";
+        $stmt = mysqli_prepare($conn, $query);
 
-if (mysqli_num_rows($result) == 1) {
-    // Authentication successful, redirect or perform login action
-    echo "Login successful!";
-} else {
-    // Authentication failed, handle accordingly
-    echo "Login failed. Invalid username or password.";
-}
+        if ($stmt) {
+            mysqli_stmt_bind_param($stmt, "ss", $username,$password);
+            mysqli_stmt_execute($stmt);
+            $result = mysqli_stmt_get_result($stmt);
+        }
+
+        if (mysqli_num_rows($result) == 1) {
+            // Authentication successful, redirect or perform login action
+            echo "Login successful!";
+        } else {
+            // Authentication failed, handle accordingly
+            echo "Login failed. Invalid username or password.";
+        }
 
 
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
+        ini_set('display_errors', 1);
+        ini_set('display_startup_errors', 1);
+        error_reporting(E_ALL);
 
-mysqli_close($conn);
+        mysqli_close($conn);
 
-?>  
-
+    ?>  
+    </body>
+</html>
